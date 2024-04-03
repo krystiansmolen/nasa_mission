36cc1d082817e64560e38960eea61caab5d3c938
Krystian Smoleń <krystian.smolen@icloud.com>
2024-04-03 17:53:46 +0200

    corrected comments

--- lib/fuel_calculation/additional_weight.ex
+++ lib/fuel_calculation/additional_weight.ex
@@ -1,35 +1,35 @@
 defmodule FuelCalculation.AdditionalWeight do
   def calculate(fuel_mass), do: calculate(fuel_mass, 0)
 
-  # Every 9278kg of fuel, mass gains 2960kg
+  # For every 9278kg of fuel, rocket gains gains 2960kg of additional fuel
   defp calculate(fuel_mass, additional_fuel_mass) when fuel_mass > 9278 do
     indicator = floor(fuel_mass / 9278)
 
     calculate(fuel_mass - (indicator * 9278), (indicator * 2960) + additional_fuel_mass)
   end
 
-  # Every 2960kg of fuel, mass gains 915kg
+  # For every 2960kg of fuel, rocket gains gains 915kg of additional fuel
   defp calculate(fuel_mass, additional_fuel_mass) when fuel_mass > 2960 do
     indicator = floor(fuel_mass / 2960)
 
     calculate(fuel_mass - (indicator * 2960), (indicator * 915) + additional_fuel_mass)
   end
 
-  # Every 915kg of fuel, mass gains 254kg
+  # For every 915kg of fuel, rocket gains gains 254kg of additional fuel
   defp calculate(fuel_mass, additional_fuel_mass) when fuel_mass > 915 do
     indicator = floor(fuel_mass / 915)
 
     calculate(fuel_mass - (indicator * 915), (indicator * 254) + additional_fuel_mass)
   end
 
-  # Every 254kg of fuel, mass gains 40kg
+  # For every 254kg of fuel, rocket gains gains 40kg of additional fuel
   defp calculate(fuel_mass, additional_fuel_mass) when fuel_mass > 254 do
     indicator = floor(fuel_mass / 254)
 
     calculate(fuel_mass - (indicator * 254), (indicator * 40) + additional_fuel_mass)
   end
 
-  # Every 40kg of fuel, mass no more gains
+  # For every 40kg of fuel, rocket gains no more gains additional fuel
   defp calculate(fuel_mass, additional_fuel_mass) when fuel_mass >= 40  do
     indicator = ceil(fuel_mass / 40)
 
