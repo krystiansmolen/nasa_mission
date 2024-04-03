380528ae9bc0b0395189a236dd97cdf5c6c2c41b
Krystian Smoleń <krystian.smolen@icloud.com>
2024-04-03 17:20:13 +0200

    renamed function

--- lib/nasa_mission.ex
+++ lib/nasa_mission.ex
@@ -2,7 +2,7 @@ defmodule NasaMission do
   alias FuelCalculation.{BaseWeight, AdditionalWeight}
   @moduledoc false
 
-  def calculate_fuel_weight(mass, coordinates) do
+  def calculate_total_fuel_weight(mass, coordinates) do
     base_weight = BaseWeight.calculate(mass, coordinates)
     additional_fuel_weight = AdditionalWeight.calculate(base_weight)
 
--- test/nasa_mission_test.exs
+++ test/nasa_mission_test.exs
@@ -3,14 +3,14 @@ defmodule NasaMissionTest do
   doctest NasaMission
 
   test "do a flight according to coordinations: launch Earth, land Moon, launch Moon, land Earth" do
-    assert NasaMission.calculate_fuel_weight(28801, [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62}, {:land, 9.807}]) == 51898
+    assert NasaMission.calculate_total_fuel_weight(28801, launch: 9.807, land: 1.62, launch: 1.62, land: 9.807) == 51898
   end
 
   test "do a flight according to coordinations: launch Earth, land Mars, launch Mars, land Earth" do
-    assert NasaMission.calculate_fuel_weight(14606, [{:launch, 9.807}, {:land, 3.711}, {:launch, 3.711}, {:land, 9.807}]) == 33388
+    assert NasaMission.calculate_total_fuel_weight(14606, launch: 9.807, land: 3.711, launch: 3.711, land: 9.807) == 33388
   end
 
   test "do a flight according to coordinations: launch Earth, land Moon, launch Moon, land Mars, launch Mars, land Earth" do
-    assert NasaMission.calculate_fuel_weight(75432, [{:launch, 9.807}, {:land, 1.62}, {:launch, 1.62}, {:land, 3.711}, {:launch, 3.711}, {:land, 9.807}]) == 212161
+    assert NasaMission.calculate_total_fuel_weight(75432, launch: 9.807, land: 1.62, launch: 1.62, land: 3.711, launch: 3.711, land: 9.807) == 212161
   end
 end
